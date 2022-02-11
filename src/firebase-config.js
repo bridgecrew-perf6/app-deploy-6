f874782fe28287024aea0c2c5a52d49b49c94d32
Jorge Garcia <jorge.garcia@skydropx.com>
2022-02-11 12:49:21 -0600

    fix(update): update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,12 +6,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtiF-MUX3HEffjKd1dmx0yI2fN8QVylK8",
-    authDomain: "skydropx-341017.firebaseapp.com",
-    projectId: "skydropx-341017",
-    storageBucket: "skydropx-341017.appspot.com",
-    messagingSenderId: "116434497431",
-    appId: "1:116434497431:web:29751f06cab7e9d6b19ed2"
+    apiKey: "AIzaSyCC4y0vGOlLwzHorO4MdDXXsE_oCgogsWI",
+    authDomain: "delfos-ds-core-invoice.firebaseapp.com",
+    projectId: "delfos-ds-core-invoice",
+    storageBucket: "delfos-ds-core-invoice.appspot.com",
+    messagingSenderId: "317168919444",
+    appId: "1:317168919444:web:96c43cf02477e41f092581",
+    measurementId: "G-N1X2L7ZWXG"
   };
 
 const app = initializeApp(firebaseConfig);
