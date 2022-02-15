0d511c7cc095db0b5c6d7aee76465fdef94672ea
jorgegpsmsky <97004834+jorgegpsmsky@users.noreply.github.com>
2022-02-15 16:45:41 -0600

    fit(update):update

--- src/App.js
+++ src/App.js
@@ -1,70 +1,74 @@
 import "./App.css";
 import { authentication } from "./firebase-config";
 import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
-import { get } from 'axios';
+import {get } from 'axios';
 
 function App() {
-  let token
+    let token
 
-  const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(authentication, provider)
-      .then((result) => {
-        if (result.user) {
-          token = result._tokenResponse.oauthIdToken;
-          console.log(`Token: ${token}`)
+    const signInWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(authentication, provider)
+            .then((result) => {
+                if (result.user) {
+                    token = result._tokenResponse.oauthIdToken;
+                    console.log(`Token: ${token}`)
+                }
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    const signOutWithGoogle = () => {
+        signOut(authentication)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    const dataGoogle = () => {
+        console.log(`auth: ${authentication}`)
+        console.log(`token: ${token}`)
+    };
+    const submit = (e) => {
+        e.preventDefault()
+        fileUpload().then((response) => {
+            console.log(response.data);
+        })
+    };
+    const fileUpload = () => {
+        const url = 'https://us-central1-personalproyect-337100.cloudfunctions.net/function-private';
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': `Bearer ${token}`
+            }
         }
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const signOutWithGoogle = () => {
-    signOut(authentication)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const dataGoogle = () => {
-    console.log(`auth: ${authentication}`)
-    console.log(`token: ${token}`)
-  };
-  const submit = (e) => {
-    e.preventDefault()
-    fileUpload().then((response) => {
-      console.log(response.data);
-    })
-  };
-  const fileUpload = () => {
-    const url = 'https://us-central1-delfos-ds-core-invoice.cloudfunctions.net/invoice-private';
-    const config = {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Authorization':`Bearer ${token}`
-      }
-    }
-    return get(url, {}, { headers : config.headers})
-  };
+        return get(url, {}, { headers: config.headers })
+    };
 
-  return (
-    <div className="App">
-      <div>
-        <button onClick={signInWithGoogle}>Sign In</button>
-        <button onClick={signOutWithGoogle}>Sign Out</button>
-        <button onClick={dataGoogle}>Get Data</button>
-      </div>
-      <div>
-        <form onSubmit={submit}>
-          <h1>File Upload</h1>
-          <button type="submit">Upload</button>
-        </form>
-      </div>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        div >
+        <
+        button onClick = { signInWithGoogle } > Sign In < /button> <
+        button onClick = { signOutWithGoogle } > Sign Out < /button> <
+        button onClick = { dataGoogle } > Get Data < /button> <
+        /div> <
+        div >
+        <
+        form onSubmit = { submit } >
+        <
+        h1 > File Upload < /h1> <
+        button type = "submit" > Upload < /button> <
+        /form> <
+        /div> <
+        /div>
+    );
 }
 
 export default App;
